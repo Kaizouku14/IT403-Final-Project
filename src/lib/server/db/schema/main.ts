9d61e5a4eecff015b44d48ae6129678b2c98b39c
kaizodevx <mandaalv72@gmail.com>
2025-11-08 13:05:55 +0800

    feat(setup): initial project setup

---
+++ src/lib/server/db/schema/main.ts
@@ -0,0 +1,3 @@
+import { sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const post = sqliteTable('post', {});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ThemeToggler from '$lib/components/theme/theme-toggler.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Hello World</h1>
+</section>
